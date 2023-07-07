42e517d1ce5672b5e52fce2844b8a128935124a7
NewAgeWarrior <farisshaikh73@gmail.com>
2023-07-07 07:37:21 +0530

    React JS Project with only frontend

---
+++ shopping-cart-react-main/src/pages/checkout/checkout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const checkout = () => {
+  return (
+    <div><h1>Sucessfully Checkout</h1></div>
+  )
+}
---
+++ shopping-cart-react-main/src/pages/contact.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Contact = () => {
+  return <div><h1>Check Out Successfully</h1></div>;
+};
